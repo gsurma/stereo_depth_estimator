ef71443085742a3839f2521a3067615c92dfbba5
Greg <grzegorz.surma@icloud.com>
2023-05-25 18:28:01 +0200

    no message

--- stereo_depth_estimator_sgbm.py
+++ stereo_depth_estimator_sgbm.py
@@ -8,11 +8,15 @@ import PIL.Image as Image
 from sklearn.preprocessing import normalize
 
 DPI=96
-DATASET = "data/1"
+DATASET = "data/6"
 DATASET_LEFT = DATASET+"/left/"
 DATASET_RIGHT = DATASET+"/right/"
 DATASET_DISPARITIES = DATASET+"/disparities/"
 DATASET_COMBINED = DATASET+"/combined/"
+DATASET_TRAINING = DATASET+"/training/"
+DATASET_VALIDATION = DATASET+"/validation/"
+DATASET_TESTING = DATASET+"/testing/"
+
 
 def process_frame(left, right, name):
 	kernel_size = 3
@@ -57,6 +61,24 @@ def create_combined_output(left, right, name):
 	combined = np.concatenate((left, right, cv2.imread(DATASET_DISPARITIES+name)), axis=0)
 	cv2.imwrite(DATASET_COMBINED+name, combined)
 
+def divide_into_parts(image):
+	width, height = image.size
+	new_width = height
+	new_height = height
+
+	c_left = (width - new_width)/2
+	c_top = (height - new_height)/2
+	c_right = (width + new_width)/2
+	c_bottom = (height + new_height)/2
+	c = image.crop((c_left, c_top, c_right, c_bottom))
+
+	r_left = c_left+new_width
+	r_top = (height - new_height)/2
+	r_right = c_right+new_width
+	r_bottom = (height + new_height)/2
+	r = image.crop((r_left, r_top, r_right, r_bottom))
+	return c, r
+
 def process_dataset():
 	left_images = [f for f in os.listdir(DATASET_LEFT) if not f.startswith('.')]
 	right_images = [f for f in os.listdir(DATASET_RIGHT) if not f.startswith('.')]
@@ -71,4 +93,54 @@ def process_dataset():
 		process_frame(left_image, right_image, left_images[i])
 
 if __name__== "__main__":
-	process_dataset()
+	# process_dataset()
+
+	# disparities = [f for f in os.listdir(DATASET_DISPARITIES) if not f.startswith('.')]
+	# disparities.sort()
+
+	lefts = [f for f in os.listdir(DATASET_LEFT) if not f.startswith('.')]
+	lefts.sort()
+
+	# assert(len(disparities)==len(lefts))
+
+	for i in range(len(lefts)):
+		left_center, left_right = divide_into_parts(Image.open(DATASET_LEFT+lefts[i]))
+		left_center = cv2.cvtColor(np.array(left_center.resize((256, 256), Image.ANTIALIAS)), cv2.COLOR_RGB2BGR)
+		#left_right = cv2.cvtColor(np.array(left_right.resize((256, 256), Image.ANTIALIAS)), cv2.COLOR_RGB2BGR)
+
+		# disparity_center, disparity_right = divide_into_parts(Image.open(DATASET_DISPARITIES+disparities[i]))
+		# disparity_center = cv2.cvtColor(np.array(disparity_center.resize((256, 256), Image.ANTIALIAS)), cv2.COLOR_RGB2BGR)
+		# disparity_right = cv2.cvtColor(np.array(disparity_right.resize((256, 256), Image.ANTIALIAS)), cv2.COLOR_RGB2BGR)
+
+		# if i % 5 == 0:
+		# 	if i % 2 == 0:
+		# 		output = DATASET_VALIDATION
+		# 	else:
+		# 		output = DATASET_TESTING
+		# else:
+		# 	output = DATASET_TRAINING
+
+		#center_concat = np.concatenate((left_center, disparity_center), axis=1)
+		cv2.imwrite(DATASET_TESTING+str(i)+".png", left_center) 
+		# right_concat = np.concatenate((left_right, disparity_right), axis=1)
+		# cv2.imwrite(output+str(i)+str((2*(i+1)))+".png", right_concat) 
+
+
+	# for i, element in enumerate(disparities):
+	# 	image_center, image_right = divide_into_parts(Image.open(DATASET_DISPARITIES+element))
+		
+	# 	image_center = image_center.resize((256, 256), Image.ANTIALIAS)
+	# 	image_center.save(DATASET_OUTPUTS+str((2*(i+1))-1)+".png")
+
+	# 	image_right = image_right.resize((256, 256), Image.ANTIALIAS)
+	# 	image_right.save(DATASET_OUTPUTS+str((2*(i+1)))+".png")
+
+
+	# for i, element in enumerate(lefts):
+	# 	image_center, image_right = divide_into_parts(Image.open(DATASET_LEFT+element))
+		
+	# 	image_center = image_center.resize((256, 256), Image.ANTIALIAS)
+	# 	image_center.save(DATASET_INPUTS+str((2*(i+1))-1)+".png")
+
+	# 	image_right = image_right.resize((256, 256), Image.ANTIALIAS)
+	# 	image_right.save(DATASET_INPUTS+str((2*(i+1)))+".png")
